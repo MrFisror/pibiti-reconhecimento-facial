afc20c534a8377e249359420eed948004c458089
MrFisror <bruno.souto1@estudante.ifgoiano.edu.br>
2026-07-02 19:36:39 -0300

    Bypass nuclear para o bug do OpenCV na nuvem

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import os
+import sys
+os.system(f"{sys.executable} -m pip uninstall -y opencv-python")
+os.system(f"{sys.executable} -m pip install opencv-python-headless")
+
 import streamlit as st
 import cv2
 from deepface import DeepFace
